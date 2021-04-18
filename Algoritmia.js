6223eda7a92f2eb3d49e3860e941de4f0af36e82
ArturoParra <72054956+ArturoParra@users.noreply.github.com>
2021-04-17 21:21:24 -0500

    Add files via upload

---
+++ Algoritmia.js
@@ -0,0 +1,48 @@
+function problema3(){
+
+    var tamanios = [];
+    var tam = [];
+
+    var palabra = document.getElementById('p3-input').value;
+
+    if(palabra!=''){
+
+        var array = palabra.split(',');
+
+      //  array.push(palabra);
+
+        for (var i = 0; i < array.length; i++){
+
+            var letras = array[i];
+
+        //    console.log(letras);
+
+            var str1Unique = [...new Set(letras.split(''))];
+
+            var result = str1Unique.length;
+            var res = str1Unique.length;
+
+        //    console.log(result);
+
+            var resultado = str1Unique.join("");
+
+        //    console.log(resultado);
+
+            console.log(str1Unique);
+
+            tamanios.push(result);
+            tam.push(result)
+
+
+
+        }
+
+        var mayor = Math.max.apply(null, tamanios)
+
+        console.log(mayor);
+
+        document.querySelector('#p3-output').textContent = "El mayor numero de caracteres es " + mayor;
+        document.querySelector('#p3-outputdos').textContent = "Los caracteres de cada palabra son: " + tam;
+    }
+
+}
